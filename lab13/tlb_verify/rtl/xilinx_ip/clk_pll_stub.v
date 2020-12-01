e4fdbbb3ada055d3a2e3729084516069050d3d4a
feiyu-byte <61310772+feiyu-byte@users.noreply.github.com>
2020-12-02 01:38:48 +0800

    lab13 done

---
+++ lab13/tlb_verify/rtl/xilinx_ip/clk_pll_stub.v
@@ -0,0 +1,19 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Wed Dec  2 01:13:29 2020
+// Host        : DESKTOP-9TJ4F4D running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub C:/tlb_verify/rtl/xilinx_ip/clk_pll_stub.v
+// Design      : clk_pll
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tfbg676-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module clk_pll(clk_out1, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_in1" */;
+  output clk_out1;
+  input clk_in1;
+endmodule
